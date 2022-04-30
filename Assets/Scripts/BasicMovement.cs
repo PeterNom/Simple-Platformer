8b55ab0bde712e503f91a6bc4fb2cce9840dd76c
Panagiotis Nomikos <peternomik@gmail.com>
2022-04-30 17:43:27 +0300

    First Commit

---
+++ Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    private Rigidbody2D playerRigidBody;
+    private bool onGround = true;
+    private Transform playerImage;
+    private Animator playerAnim;
+
+    public Transform onGroundCheck;
+    public float checkGroundRadius;
+    public LayerMask groundLayer;
+    public float speed     = 5;
+    public float jumpForce = 10;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    public bool facingRight = true;
+    public float horizontalValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidBody = GetComponent<Rigidbody2D>();
+        playerImage = GetComponent<Transform>();
+        playerAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+        BetterJump();
+        properFlip();
+        CheckIfGrounded();
+    }
+
+
+    void Move()
+    {
+        horizontalValue = Input.GetAxisRaw("Horizontal") * speed;
+
+        if( ( horizontalValue<0 || horizontalValue > 0 ) && ( onGround ) )
+        {
+            playerAnim.SetBool("Moving", true);
+        }
+        else
+        {
+            playerAnim.SetBool("Moving", false);
+        }
+
+        playerRigidBody.velocity = new Vector2(horizontalValue, playerRigidBody.velocity.y);
+    }
+
+    void Jump()
+    {
+        //Input.GetKeyDown(KeyCode.Space)
+        if (Input.GetAxis("Jump") != 0 && onGround)
+        {
+            playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, jumpForce);
+        }
+    }
+
+    void CheckIfGrounded()
+    {
+        Collider2D groundCollider = Physics2D.OverlapCircle(onGroundCheck.position, checkGroundRadius, groundLayer);
+
+        if (groundCollider == null)
+        {
+            onGround = false;
+        }
+        else
+        {
+            onGround = true;
+        }
+
+        playerAnim.SetBool("OnGround", onGround);
+    }
+
+    void BetterJump()
+    {
+        if(playerRigidBody.velocity.y < 0)
+        {
+            playerRigidBody.velocity += Vector2.up * Physics2D.gravity * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (playerRigidBody.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            playerRigidBody.velocity += Vector2.up * Physics2D.gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    void properFlip()
+    {
+        if ((horizontalValue < 0 && facingRight) || (horizontalValue > 0 && !facingRight))
+        {
+            facingRight = !facingRight;
+            playerImage.Rotate(new Vector3(0, 180, 0));
+        }
+    }
+}
---
+++ Assets/Scripts/CameraManagment.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManagment : MonoBehaviour
+{
+    private Transform myCamera;
+    private float newX, newY;
+    private bool change = false;
+    float smoothTime = 0.5f;
+    float yVelocity = 0.0f;
+
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCamera = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Mathf.Abs(player.position.x) - Mathf.Abs(myCamera.position.x) > 8 )
+        {
+            newX = player.position.x;
+            change = true;
+        }
+
+        if(change)
+        {
+            change = false;
+            float newPosition;
+            do
+            {
+                newPosition = Mathf.SmoothDamp(myCamera.position.x, player.position.x, ref yVelocity, smoothTime);
+                myCamera.position = new Vector3(newPosition, myCamera.position.y, myCamera.position.z);
+            } while (myCamera.position.x == player.position.x);
+        }
+        
+        
+    }
+}
